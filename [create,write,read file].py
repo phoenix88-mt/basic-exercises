ef4694615fe8f385037e8b8d063e5b509eaee3fa
MT <52533612+phoenix88-mt@users.noreply.github.com>
2019-07-31 14:23:35 +0300

    Update [create,write,read file].py

--- [create,write,read file].py
+++ [create,write,read file].py
@@ -5,7 +5,7 @@ f.write("apple\nbanana\ncherry\nfig")
 # And file closed in order not to let file keep memory in PC
 f.close()
 
-# In order make process file we opened above file in read (r) mode
+# In order not to make process file we opened it below in read (r) mode
 f = open("trying.txt", "r")
 print(f.read())
 f.close()
