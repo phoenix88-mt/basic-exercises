1b68c95cea52b0ad9334f1780eef58a34ea077db
MT <52533612+phoenix88-mt@users.noreply.github.com>
2019-07-31 14:32:15 +0300

    Create [first-func].py

---
+++ [first-func].py
@@ -0,0 +1,19 @@
+# encoding=utf-8
+# Defining functions
+def make_reg(name, surname, city, department):
+    print("-"*30)
+
+    print("Name      : ", name)
+    print("Surname   : ", surname)
+    print("City      : ", city)
+    print("Department: ", department)
+
+    print("-"*30)
+    
+# Defining func. finished
+
+# Outputs according to defined func.
+
+make_reg("Suzan", "Cliff", "London", "Earth Science")
+make_reg("John", "Smith", "Leeds", "Physics")
+make_reg("Ali", "Veli", "İstanbul", "History")
